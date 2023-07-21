de7720f8d7019993f3c0d6d7ed35d89d72a32153
TrongTre07 <trongtruth2003@gmail.com>
2023-07-21 18:13:45 +0700

    merge header to part 2

--- App.js
+++ App.js
@@ -1,36 +1,32 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import TestSqlite from './src/views/TestSqlite'
-import HeaderView from './src/views/HeaderView'
-import TestLocation from './src/views/TestLocation'
-import UserContext from './src/contexts/UserContext'
-import TongCucThuySanView from './src/views/TongCucThuySanView'
-import HoatDongChuyenTaiView from './src/views/HoatDongChuyenTaiView'
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import TestSqlite from './src/views/TestSqlite';
+import HeaderView from './src/views/HeaderView';
+import TestLocation from './src/views/TestLocation';
+import UserContext from './src/contexts/UserContext';
+import TongCucThuySanView from './src/views/TongCucThuySanView';
+import HoatDongChuyenTaiView from './src/views/HoatDongChuyenTaiView';
 
 const App = () => {
   return (
-   
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={{flexGrow:1}} showsVerticalScrollIndicator={false} >
-      <HeaderView/>
-        
-        <TongCucThuySanView/>
-        
-
-      <HoatDongChuyenTaiView/>
+      <ScrollView
+        contentContainerStyle={{flexGrow: 1}}
+        showsVerticalScrollIndicator={false}>
+        <HeaderView />
+        <TongCucThuySanView />
+        <HoatDongChuyenTaiView />
       </ScrollView>
     </View>
+  );
+};
 
-    
-  )
-}
-
-export default App
+export default App;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     paddingHorizontal: 12,
-    paddingTop:12,
-  }
-})
+    paddingTop: 12,
+  },
+});
