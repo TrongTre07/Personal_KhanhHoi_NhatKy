c1ebc5ce53d8cb36997906f364f52f11828528ec
ThaiHoa1402 <110773925+kurousagi110@users.noreply.github.com>
2023-07-28 20:14:44 +0700

    confirm

--- src/views/ViewPDF.js
+++ src/views/ViewPDF.js
@@ -1,14 +1,12 @@
-import React, { useContext, useEffect } from 'react';
-import { View, StyleSheet, PermissionsAndroid } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import Pdf from 'react-native-pdf';
-import { FormContext } from '../contexts/FormContext';
-import FileViewer from "react-native-file-viewer";
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
   },
   pdf: {
     flex: 1,
@@ -16,49 +14,14 @@ const styles = StyleSheet.create({
   },
 });
 
-const ViewPDF = () => {
-  const { thongTinTau } = useContext(FormContext);
-  console.log('========================', thongTinTau.dairy_name);
-
-  const pdfPath = `file:///storage/emulated/0/Documents/pdf/abc.pdf`;
-
-  useEffect(() => {
-    requestExternalStoragePermission();
-  }, []);
-
-  const requestExternalStoragePermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-        {
-          title: 'External Storage Permission',
-          message: 'This app needs access to your external storage to open PDF files.',
-          buttonPositive: 'OK',
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('External storage permission granted');
-      } else {
-        console.log('External storage permission denied');
-      }
-    } catch (err) {
-      console.warn('Error while requesting external storage permission:', err);
-    }
-  };
-
-  let yourPDFURI = { uri: pdfPath, cache: true };
-  
+const ViewPDF = (name) => { 
+  const pdf = name;
+  const localfile = { uri: '/storage/emulated/0/Android/data/com.khanhhoiapp/files/Dowload/invoice_1.pdf' };  
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <Pdf
-        ref={(pdf) => {
-          this.pdf = pdf;
-        }}
-        source={yourPDFURI}
-        style={{ flex: 1 }}
-        onError={(error) => {
-          console.log(error);
-        }}
+        source={localfile}
+        style={styles.pdf}
       />
     </View>
   );
